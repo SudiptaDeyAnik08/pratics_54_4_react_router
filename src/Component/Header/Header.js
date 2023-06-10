e548276e66172b54b4196d0bbe48b5312df90da8
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-06-11 01:46:11 +0600

    react router, tailwind and hero icon dependenciy added. And The primary navbar added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import img from '../../asstes/uni.png'
+
+function Header() {
+  return (
+    <div>
+        <div>
+            <img src={img} alt="" className='w-20'/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
